cebbcf5f456adee48db51bd4e71da703c36014b3
TechLee <techlee@qq.com>
2018-10-17 00:20:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Database adapter for php-casbin
+
+Database adapter for php-casbin.
+
+the current supported databases are:
+
+| type | database |
+| ------ | ------ |
+| mysql | PostgreSQL |
+| pgsql | MySQL |
+| sqlite | SQLite |
+| sqlsrv | SqlServer |
+
+### Installation
+
+Use [Composer](https://getcomposer.org/)
+
+```
+composer require casbin/database-adapter
+```
+
+### Usage
+
+```php
+
+require_once './vendor/autoload.php';
+
+use Casbin\Enforcer;
+use Casbin\Util\Log;
+use CasbinAdapter\Database\Adapter as DatabaseAdapter;
+
+Log::$enableLog = true;
+
+$config = [
+    'type'     => 'mysql', // mysql,pgsql,sqlite,sqlsrv
+    'hostname' => '127.0.0.1',
+    'database' => 'test',
+    'username' => 'root',
+    'password' => 'abc-123',
+    'hostport' => '3306',
+];
+
+$adapter = DatabaseAdapter::newAdapter($config);
+
+$e = new Enforcer(__DIR__ . '/examples/modelandpolicy/basic_model.conf', $adapter);
+
+$sub = "alice"; // the user that wants to access a resource.
+$obj = "data1"; // the resource that is going to be accessed.
+$act = "read"; // the operation that the user performs on the resource.
+
+if ($e->enforce($sub, $obj, $act) === true) {
+    // permit alice to read data1x
+} else {
+    // deny the request, show an error
+}
+```
+
+### Getting Help
+
+- [php-casbin](https://github.com/php-casbin/php-casbin)
---
+++ src/Adapter.php
@@ -0,0 +1,121 @@
+<?php
+namespace CasbinAdapter\Database;
+
+use Casbin\Exceptions\CasbinException;
+use Casbin\Persist\Adapter as AdapterContract;
+use TechOne\Database\Manager;
+use Casbin\Persist\AdapterHelper;
+/**
+ * DatabaseAdapter
+ * @author techlee@qq.com
+ */
+class Adapter implements AdapterContract
+{
+    use AdapterHelper;
+
+    protected $config;
+
+    protected $connection;
+
+    public $casbinRuleTableName = 'casbin_rule';
+
+    public function __construct(array $config)
+    {
+        $this->config     = $config;
+        $this->connection = (new Manager($config))->getConnection();
+        $this->initTable();
+    }
+
+    public static function newAdapter(array $config)
+    {
+        return new static($config);
+    }
+
+    public function initTable()
+    {
+        $sql = <<<EOT
+CREATE TABLE IF NOT EXISTS `$this->casbinRuleTableName` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ptype` varchar(255) NOT NULL,
+  `v0` varchar(255) DEFAULT NULL,
+  `v1` varchar(255) DEFAULT NULL,
+  `v2` varchar(255) DEFAULT NULL,
+  `v3` varchar(255) DEFAULT NULL,
+  `v4` varchar(255) DEFAULT NULL,
+  `v5` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+EOT;
+        $this->connection->execute($sql, []);
+    }
+
+    public function savePolicyLine($ptype, array $rule)
+    {
+
+        $col['`ptype`'] = $ptype;
+        foreach ($rule as $key => $value) {
+            $col['`v' . strval($key) . '`'] = $value;
+        }
+
+        $colStr = implode(', ', array_keys($col));
+
+        $name = rtrim(str_repeat('?, ', count($col)), ', ');
+
+        $sql = 'INSERT INTO `' . $this->casbinRuleTableName . '`(' . $colStr . ') VALUES (' . $name . ') ';
+
+        $this->connection->execute($sql, array_values($col));
+    }
+
+    public function loadPolicy($model)
+    {
+        $rows = $this->connection->query('SELECT * FROM `' . $this->casbinRuleTableName . '`');
+
+        foreach ($rows as $row) {
+            $line = implode(', ', array_slice(array_values($row), 1));
+            $this->loadPolicyLine(trim($line), $model);
+        }
+    }
+
+    public function savePolicy($model)
+    {
+        foreach ($model->model['p'] as $ptype => $ast) {
+            foreach ($ast->policy as $rule) {
+                $this->savePolicyLine($ptype, $rule);
+            }
+        }
+
+        foreach ($model->model['g'] as $ptype => $ast) {
+            foreach ($ast->policy as $rule) {
+                $this->savePolicyLine($ptype, $rule);
+            }
+        }
+        return true;
+    }
+
+    public function addPolicy($sec, $ptype, $rule)
+    {
+        return $this->savePolicyLine($ptype, $rule);
+    }
+
+    public function removePolicy($sec, $ptype, $rule)
+    {
+        $where['ptype'] = $ptype;
+        $condition[]    = 'ptype = :ptype';
+        foreach ($rule as $key => $value) {
+            $where['v' . strval($key)] = $value;
+            $condition[]               = 'v' . strval($key) . ' = :' . 'v' . strval($key);
+        }
+        if (empty($condition)) {
+            return;
+        }
+
+        $sql = 'DELETE FROM `' . $this->casbinRuleTableName . '` WHERE ' . implode(' AND ', $condition);
+
+        return $this->connection->execute($sql, $where);
+    }
+
+    public function removeFilteredPolicy($sec, $ptype, $fieldIndex, ...$fieldValues)
+    {
+        throw new CasbinException("not implemented");
+    }
+}
