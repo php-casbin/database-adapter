b4b956d0ca0087f5c2e1408b2d4c1922552c5818
basakest <ab1652759879@gmail.com>
2021-03-19 18:53:16 +0800

    feat: use 'Github Actions' to run tests and auto release

--- tests/AdapterPgsqlTest.php
+++ tests/AdapterPgsqlTest.php
@@ -11,7 +11,7 @@ class AdapterPgsqlTest extends AdapterTest
             'hostname' => $this->env('DB_PORT', '127.0.0.1'),
             'database' => $this->env('DB_DATABASE', 'casbin'),
             'username' => $this->env('DB_USERNAME', 'postgres'),
-            'password' => $this->env('DB_PASSWORD', ''),
+            'password' => $this->env('DB_PASSWORD', 'postgres'),
             'hostport' => $this->env('DB_PORT', 5432),
         ];
     }
