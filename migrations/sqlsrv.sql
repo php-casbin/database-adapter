58947002eaad5f61cf422e0c754de88e498ddf57
JeiHO <chechomancr@gmail.com>
2022-10-10 11:08:14 -0600

    Syntax correction for sql server

--- migrations/sqlsrv.sql
+++ migrations/sqlsrv.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS %table_name% (
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'%table_name%') AND type in (N'U'))
+begin
+  CREATE TABLE %table_name% (
   id int IDENTITY(1,1),
   ptype varchar(255) NOT NULL,
   v0 varchar(255) DEFAULT NULL,
@@ -7,4 +9,5 @@ CREATE TABLE IF NOT EXISTS %table_name% (
   v3 varchar(255) DEFAULT NULL,
   v4 varchar(255) DEFAULT NULL,
   v5 varchar(255) DEFAULT NULL
-);
+)
+end;
